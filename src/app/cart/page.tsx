0caabe1f2a0ea2ceae774f8e0ffb184dcef74519
LazeJL <67427143+SPycre@users.noreply.github.com>
2023-09-26 14:48:10 +0200

    question 1.2

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
-import { Button, ProductCardLayout, SectionContainer } from "tp-kit/components";
+import { Button, ProductCardLayout, ProductCartLine, SectionContainer } from "tp-kit/components";
 const products = PRODUCTS_CATEGORY_DATA[0].products.slice(0, 3);
+let total = 0;
 
 export default function DevCartPage() {
   return (
@@ -23,7 +24,19 @@ export default function DevCartPage() {
 
       {/* Panier */}
       <section className="w-full lg:w-1/3 space-y-8">
-				
+        {products.map((product) => (
+          <>
+          {total += product.price}
+            <ProductCartLine
+              product={product}
+              qty={1}
+              onDelete={() => {}}
+              onQtyChange={() => {}}
+            />
+          </>
+        ))}
+				<div className="font-bold flex justify-between"><span>Total : </span><span>{total} €</span></div>
+        <Button fullWidth>Commander</Button>
 				<Button variant={"outline"} fullWidth>Vider le panier</Button>
 			</section>
       {/* /Panier */}
