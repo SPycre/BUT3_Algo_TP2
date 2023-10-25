c868c151f6ce8fc267e5ad98bacf7b20b4a05c14
LazeJL <67427143+SPycre@users.noreply.github.com>
2023-10-25 14:31:28 +0200

    Question 2.2

---
+++ prisma/migrations/20231025123117_products_and_categories/migration.sql
@@ -0,0 +1,36 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" INTEGER NOT NULL,
+    "slug" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "desc" TEXT,
+    "img" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "categoryId" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "ProductCategory" (
+    "id" INTEGER NOT NULL,
+    "slug" TEXT NOT NULL,
+    "name" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_id_key" ON "Product"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_slug_key" ON "Product"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_path_key" ON "Product"("path");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ProductCategory_id_key" ON "ProductCategory"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ProductCategory_slug_key" ON "ProductCategory"("slug");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "ProductCategory"("id") ON DELETE CASCADE ON UPDATE CASCADE;
