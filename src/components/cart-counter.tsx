d53dde67bee566d568281f52aa35fc8b36c139ef
LazeJL <67427143+SPycre@users.noreply.github.com>
2023-10-25 14:14:26 +0200

    TP3 START

--- src/components/cart-counter.tsx
+++ src/components/cart-counter.tsx
@@ -2,7 +2,6 @@ import { useStore } from "../hooks/use-cart"
 
 
 export default function CartCounter() {
-    console.log("render count")
     const count = useStore((state) => state.count)
     return (
         <>{count}</>
