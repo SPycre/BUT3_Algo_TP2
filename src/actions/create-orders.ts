1908018c30cce8685149df4e779076ca3a3e1faa
LazeJL <67427143+SPycre@users.noreply.github.com>
2023-11-28 17:25:59 +0100

    question 2.10

--- src/actions/create-orders.ts
+++ src/actions/create-orders.ts
@@ -1,29 +1,41 @@
 "use server"
 
-import { clearCart } from "../hooks/use-cart";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { ProductLineData } from "../types";
 import prisma from "../utils/prisma";
+import { getUser } from "../utils/supabase";
+import { cookies } from "next/headers";
 
 export async function createOrder(lines : ProductLineData[]) {
 
     let total = 0;
     lines.forEach((line) => total += line.qty * line.product.price)
 
-    if (lines.length > 0) {
-        let result = await prisma.order.create({
-            data : {
-                createdAt: new Date(),
-                lines: {
-                    create: lines.map((line) => {
-                        return {
-                            productId: line.product.id,
-                            subtotal: line.product.price*line.qty
-                        }
-                    })
-                },
-                total: total
-            }
-        });
-    
+    const supabase = createServerComponentClient({ cookies })
+    const user = await getUser(supabase)
+
+    if (user) {
+        if (lines.length > 0) {
+            await prisma.order.create({
+                data : {
+                    userId: user.id,
+                    createdAt: new Date(),
+                    lines: {
+                        create: lines.map((line) => {
+                            return {
+                                productId: line.product.id,
+                                subtotal: line.product.price*line.qty
+                            }
+                        })
+                    },
+                    total: total
+                }
+            });
+        
+        }
+        return {success:true}
+    } else {
+        return {error:"Utilisateur non connecté",success:false}
     }
+    
 }
--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -1,12 +1,15 @@
-import { Button, FormattedPrice, ProductCartLine } from "tp-kit/components"
+import { Button, FormattedPrice, NoticeMessage, NoticeMessageData, ProductCartLine } from "tp-kit/components"
 import { clearCart, computeCartTotal, removeLine, updateLine, useStore } from "../hooks/use-cart"
 import CartCounter from "./cart-counter"
 import { createOrder } from "../actions/create-orders"
+import { useState } from "react"
 
 export default function Cart() {
+    const [notice, setNotice] = useState<NoticeMessageData | null>()
     const lines = useStore((state) => state.lines)
     return (
         <>
+        {notice ? <NoticeMessage {...notice} onDismiss={() => setNotice(null)}/> : "" }
         {lines.map((line) => (
                 <ProductCartLine
                 className="m-1"
@@ -18,9 +21,14 @@ export default function Cart() {
                 />
             ))}
 			<div className="font-bold flex justify-between"><FormattedPrice price={computeCartTotal(lines)}/></div>
-            <Button className="m-1" fullWidth onClick={() => {
-                createOrder(lines)
-                clearCart()
+            <Button className="m-1" fullWidth onClick={async() => {
+                const result = await createOrder(lines)
+                if (result.success) {
+                    clearCart()
+                } else {
+                    setNotice({type:"error", message: result.error})
+                }
+                
             }}>Commander</Button>
 			<Button className="m-1" variant={"outline"} onClick={() => clearCart()} fullWidth>Vider le panier</Button>
         </>  
